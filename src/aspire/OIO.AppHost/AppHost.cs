be8ad4da217b566dc5b95e404c7a42de9d67fce9
newlsun <nightfury455@gmail.com>
2026-03-02 16:24:33 +0700

    configue aspire and github workflow

--- src/aspire/OIO.AppHost/AppHost.cs
+++ src/aspire/OIO.AppHost/AppHost.cs
@@ -26,6 +26,7 @@ var api = builder.AddProject<Projects.OIO_Api>("oio-api")
     .PublishAsDockerComposeService((resource, service) =>
     {
         service.Name = "oio-api";
+        service.Ports.Add("8080:8080");
 
         // Lấy các biến môi trường đã được truyền từ GitHub Actions workflow
         var endpoint = Environment.GetEnvironmentVariable("REGISTRY_ENDPOINT") ?? "ghcr.io";
