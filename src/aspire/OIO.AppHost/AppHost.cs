f4de40e69b7e5eadfe6edcf00335b225ee83509c
newlsun <nightfury455@gmail.com>
2026-03-02 15:12:07 +0700

    configue http port for aspire

--- src/aspire/OIO.AppHost/AppHost.cs
+++ src/aspire/OIO.AppHost/AppHost.cs
@@ -52,6 +52,7 @@ var db = builder.AddPostgres("database")
 
 builder.AddProject<Projects.OIO_Api>("oio-api")
     // .WithHttpHealthCheck("/health")
+    .WithHttpEndpoint(port: 8080, targetPort: 8080, name: "http")
     .WithReference(db, connectionName: "Database")
     .WaitFor(db)
     .PublishAsDockerComposeService((resource, service) =>
